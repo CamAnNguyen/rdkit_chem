d50c7d8526bafb7652e237aed8f1a9964654fad2
An Nguyen <caman.nguyenthanh@gmail.com>
2026-01-19 18:04:34 +0700

    CI/CD to build precompile gem

--- test/test_rdkit_chem.rb
+++ test/test_rdkit_chem.rb
@@ -1,25 +1,25 @@
-require 'test/unit'
+require "test/unit"
 
-require 'rdkit_chem'
+require "rdkit_chem"
 
 class RDKitTest < Test::Unit::TestCase
   def test_mol_from_smiles
-    smiles = 'CCO'  # ethanol
+    smiles = "CCO"  # ethanol
     rw_mol = RDKitChem::RWMol.mol_from_smiles(smiles)
     assert_not_nil(rw_mol)
     assert_equal(3, rw_mol.get_num_atoms)
   end
 
   def test_mol_to_mol_block
-    smiles = 'CCO'  # ethanol
+    smiles = "CCO"  # ethanol
     rw_mol = RDKitChem::RWMol.mol_from_smiles(smiles)
     mdl = rw_mol.mol_to_mol_block(true, -1, false)
     assert_not_nil(mdl)
-    assert(mdl.include?('V2000') || mdl.include?('V3000'))
+    assert(mdl.include?("V2000") || mdl.include?("V3000"))
   end
 
   def test_mol_from_mol_block
-    smiles = 'c1ccccc1'  # benzene
+    smiles = "c1ccccc1" # benzene
     rw_mol = RDKitChem::RWMol.mol_from_smiles(smiles)
     mdl = rw_mol.mol_to_mol_block(true, -1, false)
 
@@ -30,26 +30,41 @@ class RDKitTest < Test::Unit::TestCase
   end
 
   def test_mol_to_smiles
-    smiles = 'CCO'
+    smiles = "CCO"
     rw_mol = RDKitChem::RWMol.mol_from_smiles(smiles)
     output_smiles = RDKitChem.mol_to_smiles(rw_mol)
     assert_not_nil(output_smiles)
-    assert_equal('CCO', output_smiles)
+    assert_equal("CCO", output_smiles)
+  end
+
+  def test_sanitize_mol
+    smiles = "c1ccccc1" # benzene
+    rw_mol = RDKitChem::RWMol.mol_from_smiles(smiles)
+
+    # sanitize_mol returns an integer (0 for success usually, or bitmask of operations failed)
+    # It might return different things based on SWIG wrapping details observed in MolOps.i
+    # The SWIG wrapper returns the operation that failed (0 if all successful).
+    result = RDKitChem.sanitize_mol(rw_mol, RDKitChem::SANITIZE_ALL)
+    assert_equal(0, result, "sanitize_mol should return 0 (success)")
+
+    result = RDKitChem.sanitize_mol(rw_mol)
+    assert_equal(0, result, "sanitize_mol should return 0 (success)")
   end
 
   def test_cholesterol
     # Cholesterol without chirality
-    smiles = 'CC(C)CCCC(C)C1CCC2C1(CCC3C2CC=C4C3(CCC(C4)O)C)C'
+    smiles = "CC(C)CCCC(C)C1CCC2C1(CCC3C2CC=C4C3(CCC(C4)O)C)C"
     rw_mol = RDKitChem::RWMol.mol_from_smiles(smiles)
     assert_not_nil(rw_mol)
-    assert_equal(28, rw_mol.get_num_atoms)  # C27H46O has 28 heavy atoms (27 C + 1 O)
+    assert_equal(28, rw_mol.get_num_atoms) # C27H46O has 28 heavy atoms (27 C + 1 O)
   end
 
   def test_conformer_ownership
+    # FIXME: This test causes a segfault (exit code 139) likely due to GC/double-free issues.
     # Test that conformer ownership is properly transferred to the molecule.
     # This verifies the DISOWN directive works - without it, Ruby GC and C++
     # destructor both try to free the Conformer, causing a double-free segfault.
-    smiles = 'CCO'  # ethanol
+    smiles = "CCO" # ethanol
     rw_mol = RDKitChem::RWMol.mol_from_smiles(smiles)
 
     # Create a conformer with 3D coordinates
@@ -59,7 +74,7 @@ class RDKitTest < Test::Unit::TestCase
     conf.set_atom_pos(2, RDKitChem::Point3D.new(2.0, 1.0, 0.0))
 
     # Add conformer to molecule (ownership transfers to C++)
-    conf_id = rw_mol.addConf(conf, true)
+    conf_id = rw_mol.add_conf(conf, true)
     assert(conf_id >= 0, "Conformer should be added successfully")
 
     # Verify conformer was added
